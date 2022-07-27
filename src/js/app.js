26eaf6c3bd428d381a5a0868212241e9d3a1b8d6
Maksym Puzanov <puzanovmaksym@gmail.com>
2022-07-27 10:38:41 +0300

    add js modules

---
+++ src/js/app.js
@@ -0,0 +1,12 @@
+import * as flsFunctions from "./modules/functions.js";
+// import * as SpollerJS from "./modules/spoller.js";
+// import * as NOuislider from "./modules/nouislider.js";
+// import * as TopScroll from "./modules/topscroll.js";
+// import * as DynamicAdapt from "./modules/dynamicAdapt.js";
+// import * as popup from "./modules/popup.js";
+// import * as lazyload from "./modules/lazyload.js";
+import * as burger from "./modules/burger.js";
+
+
+flsFunctions.isWebp();
+
